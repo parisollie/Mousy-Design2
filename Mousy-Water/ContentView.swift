0f41904423bdefa5c50f8e336a9c48bf1f34888d
parisollie <pjff18@gmail.com>
2023-10-14 00:55:09 -0600

    Initial Commit

---
+++ Mousy-Water/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mousy-Water
+//
+//  Created by Paul Jaime Felix Flores on 14/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mousy-Water/Mousy_WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mousy_WaterApp.swift
+//  Mousy-Water
+//
+//  Created by Paul Jaime Felix Flores on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Mousy_WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
